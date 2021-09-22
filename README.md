5aaee800db28aa29cdff7cd8a26c53eb348fb4f0
omer <omernaim8@gmail.com>
2021-09-22 14:06:54 +0300

    cart efficacy change

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# Ecom store project
+# Ecom store Backend project
 Hi im OMER,<br>
-this is my ecom store project,<br>
-dynamic desktop web application,<br>
+This is my ecom store Backend project,<br>
+Dynamic desktop web application,<br>
 using DB for extracting information,<br>
 offers video games and movies for sell,<br>
 purchase using paypal api.<br>
 implemented shopping cart feature,<br>
 and admin logging option.<br>
-this project developed using PHP,<br>
-using MySQL for DB and git for version control,<br>
+In this project I developed the backend part using PHP,<br>
+iv used MySQL database management system,<br>
+git for version control,<br>
 XAMPP for testing local web.<br>
 
 enjoy<br>
--- public/cart.php
+++ public/cart.php
@@ -1,44 +1,64 @@
 <?php require_once("../resources/config.php"); ?>
- <?php
+<?php
 
+/* clicks Add to cart
+* extracting the product id from the url using GET
+* extracting product details using product id from DB
+* verifing that user didnt reach product quantity limit (limit is on DB, user quantity on session)
+* +1 on user product quantity
+* redirect to checkout page
+*/
 if(isset($_GET['add'])){
     $query= query("SELECT * FROM products WHERE product_id =".escape_string($_GET['add'])." ");
     confirm($query);
-    while($row=fetch_array($query)){
-        if ($row['product_quantity'] != $_SESSION['product_'. $_GET['add']]){ #error is quantity reach product stock limit
-            $_SESSION['product_'. $_GET['add']]+=1;
-        }
-        else{
-            set_message("Sorry, limited stock,<br> only ". $row['product_quantity'] ." ". $row['product_title']." ". "available");
-        }
-        redirect("checkout.php");
+    $row=fetch_array($query);
+    if ($row['product_quantity'] != $_SESSION['product_'. $_GET['add']]){ #error is quantity reach product stock limit
+        $_SESSION['product_'. $_GET['add']]+=1;
     }
+    else{
+        set_message("Sorry, limited stock,<br> only ". $row['product_quantity'] ." ". $row['product_title']." ". "available");
+    }
+    redirect("checkout.php");
 }
 
+/* user click Remove
+* -1 on product session, free memory if quantity reach 0
+*/
 if(isset($_GET['remove'])){
     $_SESSION['product_'. $_GET['remove']]-=1;
+    if($_SESSION['product_'. $_GET['remove']]==0)
+        unset($_SESSION['product_'. $_GET['remove']]);
     redirect("checkout.php");
 }
 
+/* user click Delete
+* free this product quantity session
+*/
 if(isset($_GET['delete'])){
-    $_SESSION['product_'. $_GET['delete']]='0';
+    unset($_SESSION['product_'. $_GET['delete']]);
     redirect("checkout.php");
 }
 
+/* presenting user cart products
+ * running on all the session array
+ * looking for products
+ * extracting product DB info
+ * presenting on cart
+ */
 function show_cart_products()
 {
-    $query = query("SELECT * FROM products");
-    confirm($query);
-    while ($row = fetch_array($query)) {
-        if(isset($_SESSION['product_'.$row['product_id']])){
-            $user_quantity = $_SESSION['product_'.$row['product_id']];
-            if ($user_quantity > 0){
-                $sub_total = $user_quantity * $row['product_price'];
-                $products = <<<DELIMETER
+    foreach ($_SESSION as $name => $user_product_quantity){
+        if(strpos($name, 'product_')!==false && $user_product_quantity > 0){    //strpos: includes
+            $product_id = explode("product_",$name)[1]; //explode: split the if from the product_X
+            $query = query("SELECT * FROM products WHERE product_id= '{$product_id}'");
+            confirm($query);
+            $row = fetch_array($query);
+            $sub_total = $user_product_quantity * $row['product_price'];
+            $products = <<<DELIMETER
                     <tr>
                     <td>$row[product_title]</td>
                     <td>$row[product_price]</td>
-                    <td>$user_quantity</td>
+                    <td>$user_product_quantity</td>
                     <td>$sub_total</td>          
                     <td><a class='btn btn-success' href="cart.php?add={$row['product_id']}"><span class='glyphicon glyphicon-plus'></span></span>Add</a></td>
                     <td><a class='btn btn-warning' href="cart.php?remove={$row['product_id']}"><span class='glyphicon glyphicon-minus'></span></span>Remove</a></td>
@@ -47,7 +67,6 @@ function show_cart_products()
                 DELIMETER;
                 echo $products;
             }
-        }
     }
 }
 
