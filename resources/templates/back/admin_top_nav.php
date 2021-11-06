e5c62c1ceaa5cb4b59c4e3f204831b41c5959d2f
omer <omernaim8@gmail.com>
2021-11-06 11:59:19 +0200

    admin side nav links redirect with GET

--- resources/templates/back/admin_top_nav.php
+++ resources/templates/back/admin_top_nav.php
@@ -1,19 +1,18 @@
-
-
 <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">Admin menu</a>
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="index.php">Admin menu</a>
 </div>
 
 <!-- Top Menu Items -->
 <ul class="nav navbar-right top-nav">
     <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b
+                    class="caret"></b></a>
         <ul class="dropdown-menu">
 
             <li class="divider"></li>
